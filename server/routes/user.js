e16888903c8dd36fd30ee5e4b9b960f842639448
Luiz Cláudio <danellaclaudioluiz@gmail.com>
2023-01-17 17:30:23 -0300

    added reset password functionality and send email

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { resetPassword, changePassword, forgotPassword, updateUser, loginStatus, logoutUser, loginUser, registerUser, getUser } = require("../controllers/user");
+const { resetPassword, changePassword, forgotPassword, updateUser, loginStatus, logout, loginUser, registerUser, getUser } = require("../controllers/user");
 const protect = require("../middleware/auth");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/logout", logoutUser);
+router.get("/logout", logout);
 router.get("/getUser", protect, getUser);
 router.get("/loggedin", loginStatus);
 router.patch("/updateuser", protect, updateUser);
 router.patch("/changepassword", protect, changePassword);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resetToken", resetPassword);
-
 
 
 module.exports = router;
